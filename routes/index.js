cb4fa89b4653d6ab5a045afa15a12c02bbcd30c9
Jesse Cravens <jesse.cravens@frogdesign.com>
2012-11-14 23:49:12 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){	
+  console.log("res.profile: " + res.profile);
+
+    switch (res.profile) {
+
+       case "smartPhone":
+          res.render('smartphone', { title: 'ResponderJS - Smart Phone', desc: 'Node.js Adaptive/Responsive Utility Framework' });
+          break;
+
+       case "tablet":
+          res.render('tablet', { title: 'ResponderJS - Tablet', desc: 'Node.js Adaptive/Responsive Utility Framework' });
+          break;
+
+       case "desktop":
+          res.render('desktop', { title: 'ResponderJS - Desktop', desc: 'Node.js Adaptive/Responsive Utility Framework' });
+          break;
+
+       case "largeScreen":
+          res.render('largescreen', { title: 'ResponderJS - Large Screen', desc: 'Node.js Adaptive/Responsive Utility Framework' });
+          break;
+
+       default:
+          //
+    }
+};
